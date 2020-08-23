c158555263bdabccc6b631ea95caffd030a60266
shyam <shyam.singh2706@gmail.com>
2020-08-23 18:48:34 +0530

    Updated File

--- Data_Structure/Extend_Class_Print_Management_Tree.py
+++ Data_Structure/Extend_Class_Print_Management_Tree.py
@@ -1,5 +1,7 @@
 ''' Extend TreeNode class built in "Create_General_Tree so that it takes designation in data part of TreeNode class.
-   Also extend print_tree function such that it can print either name tree, designation tree or name and designation tree. '''
+   Also extend print_tree function such that it can print either name tree, designation tree or name and designation tree.
+   Further Extend print_tree function such that Print Hierarchy function will print hierarchy by Levels
+'''
 
 import Create_General_Tree as CGT
 
@@ -33,8 +35,21 @@ class Management_tree(CGT.TreeNode):
         for child in self.child_node:
             child.print_tree(print_type)
 
+class Management_tree_by_level(Management_tree):
+
+    def print_tree(self, level):
+
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__"
+        print(prefix + self.node_name + ' ( ' + self.designation + ' )')
+
+        if ( self.get_level() < level ) :
+            for child in self.child_node:
+                child.print_tree(level)
+
 def Build_tree():
 
+        ''' Print by Name, Designation or Both '''
         Nilupul = Management_tree("Nilupul","CEO")
         Chinmay = Management_tree("Chinmay","CTO")
         Nilupul.add_child(Chinmay)
@@ -47,6 +62,20 @@ def Build_tree():
         Vishwa.add_child(Abhijeet)
         Anupam.add_child(Peter)
         Nilupul.print_tree("N")
+
+        ''' Print by Level '''
+        Nilupul = Management_tree_by_level("Nilpul","CEO")
+        Chinmay = Management_tree_by_level("Chinmay", "CTO")
+        Nilupul.add_child(Chinmay)
+        Vishwa = Management_tree_by_level("Vishwa","Tech Artch.")
+        Anupam = Management_tree_by_level("Anupam","HR")
+        Chinmay.add_child(Vishwa)
+        Chinmay.add_child(Anupam)
+        Peter = Management_tree_by_level("Peter","Policy HR")
+        Abhijeet = Management_tree_by_level("Abhijeet","Application Owner")
+        Vishwa.add_child(Abhijeet)
+        Anupam.add_child(Peter)
+        Nilupul.print_tree(3)
 
 if __name__ == "__main__" :
 
