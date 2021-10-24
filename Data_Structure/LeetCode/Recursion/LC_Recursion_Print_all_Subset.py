d939b2a5bf319f636efb09489c7d7aa9239d990b
shyamsingh2706 <66989908+shyamsingh2706@users.noreply.github.com>
2021-10-24 12:30:27 +0530

    Add files via upload

---
+++ Data_Structure/LeetCode/Recursion/LC_Recursion_Print_all_Subset.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/subsets/
+# https://www.tutorialcup.com/leetcode-solutions/subset-leetcode.htm
+
+#Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+#             op    ip
+#             ""   "ab"
+#       op    ip            op          ip
+#      " "   "b"           "a"         "b"   --> a is not considered in left decision tree but considered in right decision tree
+#    op    ip   op      op    ip    op     ip
+#   " "   " "  "b"     "a"    " "  "ab"   " " --> for each element , either its not considered in left decision tree or its considered in right decision tree
+
+def print_all_subset(arr,output_arr=[],op=''):
+
+
+    if len(arr) == 0 :
+        output_arr.append([op])
+        return
+
+    op1 = op  # initialize Both Output of decesion tree as initial output string # for the decesion tree where we are not considering the first element
+    op2 = op  # initialize Both Output of decesion tree as initial output string # for the decesion where we are considering the first element
+
+    op2 = op2 + str(arr[0])  # initialize Output2 string with 0th Index as we are considering first element in array and remaining element has to go through decesion tree
+    arr= arr[1:] # reset array with remaining element
+    print_all_subset(arr,output_arr,op1)
+    print_all_subset(arr,output_arr,op2)
+
+    return output_arr
+
+def main():
+
+    arr = [1,2,3]
+    print(print_all_subset(arr))
+
+if __name__ == "__main__":
+    main()
