a7f49980b39cfc42adc512a72d20f0fa408e498a
shyamsingh2706 <66989908+shyamsingh2706@users.noreply.github.com>
2021-11-09 13:10:30 +0530

    Update LC_LL_Detect_Cycle_in_LinkedList.py

--- Data_Structure/LeetCode/Linked_list/LC_LL_Detect_Cycle_in_LinkedList.py
+++ Data_Structure/LeetCode/Linked_list/LC_LL_Detect_Cycle_in_LinkedList.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/linked-list-cycle/
+# https://leetcode.com/problems/linked-list-cycle-ii/
+
 
 #  Given head, the head of a linked list, determine if the linked list has a cycle in it.
 # There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
@@ -14,6 +16,7 @@ class node():
         self.data = data
         self.next = next
 
+# https://leetcode.com/problems/linked-list-cycle/
 def hasCycle(head):
 
     fast = head
@@ -37,6 +40,47 @@ def hasCycle(head):
 
     return False
 
+# https://leetcode.com/problems/linked-list-cycle-ii/
+# Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+def detectCycle(head):
+
+    fast = head
+    slow = head
+    entry = head
+    has_cycle = False
+
+    if head is None or head.next is None :
+        return False
+
+    # using flood cycle detection
+    # move slow pointer by 1 and fast pointer by 3
+    # if there is a cycle, it will collide at some point else it wont
+    while slow and fast :
+
+        slow = slow.next
+        fast = fast.next
+        if fast:
+            fast = fast.next
+
+        if fast == slow:
+            has_cycle = True
+            break
+
+    # if it has cycle, start moving slow and entry pointer toghether, where ever they collide
+    # will be entry point of cycle
+    if has_cycle :
+
+        while slow and entry :
+
+            if slow == entry :
+                return slow.data
+
+            slow = slow.next
+            entry = entry.next
+    else:
+
+        return None
+
 def main() :
 
     node1 = node(4)
@@ -55,6 +99,9 @@ def main() :
 
     res = hasCycle(node1)
     print("given linked list has cycle : " , res)
+
+    cycle_node = detectCycle(node1)
+    print("given linked list has cycle and strting node of cycle is : ", cycle_node)
 
 if __name__  == '__main__':
     main()
