5ade5a9d01bc5baac2233c349cdaa0e247755188
shyamsingh2706 <66989908+shyamsingh2706@users.noreply.github.com>
2021-12-12 16:04:34 +0530

    Update Leetcode_Allocate_mailBox.py

--- Data_Structure/LeetCode/Random/Leetcode_Allocate_mailBox.py
+++ Data_Structure/LeetCode/Random/Leetcode_Allocate_mailBox.py
@@ -1,47 +1,54 @@
 
 # https://leetcode.com/problems/allocate-mailboxes/
 # https://leetcode.com/problems/allocate-mailboxes/discuss/1513947/The-explanation-you've-been-waiting-for
-import sys
 
-def loop (houses, start , end , num_mailboxes ) :
+# Optimized Approach through storing into Hashmap ( dict )
+class Solution(object):
+    def minDistance(self, houses, k):
+        """
+        :type houses: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l = len(houses)
+        if k >= l:
+            return 0
 
-    num_houses = end - start + 1
-    dist = 0
+        houses.sort()
 
-    if num_houses == num_mailboxes:
-        return 0
-    if num_mailboxes == 1:
-        if num_houses % 2 == 0:
-            mid = start + num_houses // 2
-            pos = houses[mid - 1] + (houses[mid] - houses[mid - 1]) // 2
-        else:
-            mid = start + (num_houses - 1) // 2
-            pos = houses[mid]
+        results = {}
 
-        return sum(abs(pos - houses[i]) for i in range(start, end + 1))
+        def findResult(start, end, r):
+            if end - start <= r:
+                return 0
+            if (start, end, r) in results:
+                return results[(start, end, r)]
 
+            if r == 1:  # Calculate total distance for one subarray
+                left = start
+                right = end - 1
+                current_result = 0
+                while left < right:
+                    current_result += houses[right] - houses[left]
+                    right -= 1
+                    left += 1
+            else:
+                current_result = 2147483647
+                for i in range(start + 1, end):
+                    current_result = min(current_result, findResult(start, i, 1) + findResult(i, end, r - 1))
 
-    min_dist = sys.maxsize
-    for i in range(start, len(houses) - num_mailboxes + 1):
-        dist = loop(houses,start, i, 1) + loop(houses,i + 1, end, num_mailboxes - 1)
-        min_dist = min(min_dist, dist)
+            results[(start, end, r)] = current_result
+            return current_result
 
-    return min_dist
-
-def minDistance( houses , k ):
-
-    houses.sort()
-    Min_dist =  loop( houses, 0 , len(houses) - 1 , k)
-
-    return Min_dist
+        return findResult(0, l, k)
 
 
 def main():
 
     houses = [1,4,8,10,20]
     k = 3
-
-    Min_dist = minDistance(houses,k)
+    s = Solution()
+    Min_dist = s.minDistance(houses,k)
 
     print("Min Ditance is :", Min_dist)
 
